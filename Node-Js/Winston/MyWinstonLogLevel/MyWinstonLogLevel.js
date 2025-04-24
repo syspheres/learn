9181ca1ee408afec00d8966f354b71a6e6d5311f
SySpheres <syspheres@gmail.com>
2025-04-24 19:48:58 +0200

    MyWinstonLogLevel V2

--- Node-Js/Winston/MyWinstonLogLevel/MyWinstonLogLevel.js
+++ Node-Js/Winston/MyWinstonLogLevel/MyWinstonLogLevel.js
@@ -3,10 +3,10 @@
 
 const NodeModule = "D:/Project/syspheres/xspheres/learn/Node-Js/Winston/node_modules";
 
-const Winston = require (NodeModule+"/winston");                       // Constante declaration for use winston packa
+const Winston = require (NodeModule+"/winston");                    // Constante declaration for use winston packa
 
 // You can change level 
-const MyLogLevel = {                                                // Constante for define my log level 
+const MyLogLevels = {                                                // Constante for define my log level 
     fatal: 0,                                                       // Fatal, abnormal situation, program must be stop
     error: 1,                                                       // Error, abnormal situation, program can handle this error 
     warn: 2,                                                        // Warning, Abnormal situation but program continue
@@ -15,9 +15,20 @@ const MyLogLevel = {                                                // Constante
     trace: 5,                                                       // Display a situation point every 5 lines of code
 };
 
+const MyLogLevel = process.argv[2];                                 // Retrieve the first argument of the program call.
+                                                                    // This argument corresponds to the default log level to use.
+
 // Create a logger Winston
 const Logger = Winston.createLogger ({                              // Create a logger
-    level: 'info',                                                  // for level infos and smaller level
+    levels: MyLogLevels,
+    level: MyLogLevel || 'info',                                                  // for level infos and smaller level
     format: Winston.format.json(),                                  // format Display log in Json
     transports: [new Winston.transports.Console()],                 // Create mthode output logging
 });
+
+Logger.fatal("fatal Message");                                     // Display on output the log message of fatal level
+Logger.error("error Message");                                     // Display on output the log message of error level
+Logger.warn("Warn Message");                                       // Display on output the log message of warning level
+Logger.info("Info Message");                                       // Display on output the log message of info level
+Logger.debug("Debug Message");                                     // Display on output the log message of debug level
+Logger.trace("Trace Message");                                     // Display on output the log message of trace level
